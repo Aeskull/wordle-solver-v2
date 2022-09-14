c2cceaabe76b5534ac40cee74d981e2e62788456
Aeskul Styr <97549569+Aeskull@users.noreply.github.com>
2022-09-14 15:39:34 -0600

    Done

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn menu(items: &[String]) -> State {
         "UNKNOWNS" => UNKNOWN,
         "NONS" => NON,
         "SOLVE" => SOLVING,
+        "CLEAR" => CLEAR,
         "EXIT" => EXIT,
         _ => EXIT,
     }
@@ -23,14 +24,16 @@ enum State {
     UNKNOWN,
     NON,
     SOLVING,
+    CLEAR,
     EXIT
 }
 
 fn main() {
-    let m_items = ["KNOWNS".to_owned(), 
-    "UNKNOWNS".to_owned(), 
-    "NONS".to_owned(), 
-    "SOLVE".to_owned(), 
+    let m_items = ["KNOWNS".to_owned(),
+    "UNKNOWNS".to_owned(),
+    "NONS".to_owned(),
+    "SOLVE".to_owned(),
+    "CLEAR".to_owned(),
     "EXIT".to_owned()];
 
     let mut known = "*****".to_owned();
@@ -42,36 +45,54 @@ fn main() {
             State::KNOWN => {
                 match solver::update_known(&mut known) {
                     Ok(_) => {
-                        println!("The new knowns: \"{}\"", known);
+                        println!("The new knowns: \"{}\"\n", known);
                     },
                     Err(_) => {
-                        println!("The knowns are still \"{}\"", known);
+                        println!("The knowns are still \"{}\"\n", known);
                     },
                 };
             },
             State::UNKNOWN => {
                 match solver::update_unknown_nons(&mut unknown, false) {
                     Ok(_) => {
-                        println!("The new unknowns: \"{}\"", unknown);
+                        println!("The new unknowns: \"{}\"\n", unknown);
                     }
                     Err(_) => {
-                        println!("THe unknowns are still \"{}\"", unknown);
+                        println!("THe unknowns are still \"{}\"\n", unknown);
                     }
                 };
             },
             State::NON => {
                 match solver::update_unknown_nons(&mut non, true) {
                     Ok(_) => {
-                        println!("The new nons are: \"{}\"", non);
+                        println!("The new nons are: \"{}\"\n", non);
                     }
                     Err(_) => {
-                        println!("The nons are still \"{}\"", non);
+                        println!("The nons are still \"{}\"\n", non);
                     }
                 };
             },
-            State::SOLVING => {},
+            State::CLEAR => {
+                known = "*****".to_owned();
+                unknown = String::new();
+                non = String::new();
+                println!("Stored Values Cleared!\n");
+            }
+            State::SOLVING => {
+                let result = solver::solve(&known, &unknown, &non);
+                if let Some(e) = result {
+                    if e.len() > 0 {
+                        println!("Results:");
+                        e.iter().for_each(|s| println!("   > {s}"));
+                        println!("There are {} possible answers\n", e.len());
+                    } else {
+                        println!("No Results!\n");
+                    }
+                }
+            },
             _ => break,
         };
         state = menu(&m_items);
+        println!();
     }
 }
--- src/solver.rs
+++ src/solver.rs
@@ -1,4 +1,4 @@
-use std::io::stdin;
+use std::{io::{stdin, BufReader, BufRead}, fs::File};
 use inquire::Select;
 
 pub fn update_known(inp: &mut String) -> Result<(), ()> {
@@ -9,6 +9,14 @@ pub fn update_known(inp: &mut String) -> Result<(), ()> {
     stdin().read_line(&mut new).unwrap();
     new = new.trim().to_owned();
 
+    if new.len() > 5 {
+        new = new.as_str()[0..=4].to_owned();
+    }
+    while new.len () < 5 {
+        new.push('*');
+    }
+
+    new = new.to_lowercase();
     println!("Is this what you want: \"{new}\"?");
     let m_items = ["YES".to_owned(), "NO".to_owned()];
 
@@ -19,6 +27,8 @@ pub fn update_known(inp: &mut String) -> Result<(), ()> {
         "NO" => return Err(()),
         _ => return Err(()),
     };
+
+    println!();
 
     Ok(())
 }
@@ -54,7 +64,9 @@ pub fn update_unknown_nons(inp: &mut String, non: bool) -> Result<(), ()> {
         }
     }
 
+    rms = rms.to_lowercase();
     rms.chars().for_each(|c| con.remove_matches(c));
+    con = con.to_lowercase();
 
     println!("Is this what you want: \"{con}\"?");
     let m_items = ["YES".to_owned(), "NO".to_owned()];
@@ -67,9 +79,62 @@ pub fn update_unknown_nons(inp: &mut String, non: bool) -> Result<(), ()> {
         _ => return Err(()),
     };
 
+    println!();
+
     Ok(())
 }
 
-pub fn solve(known: &String, unknown: &String, nons: &String) -> String {
-    todo!();
+pub fn solve(known: &String, unknown: &String, nons: &String) -> Option<Vec<String>> {
+    let file = match File::open("list.txt") {
+        Ok(e) => e,
+        Err(_) => {
+            println!("\nRequisite resource file 'list.txt' not found\n");
+            return None;
+        }
+    };
+
+    let mut list = BufReader::new(file)
+    .lines()
+    .map(|s| s.unwrap().trim().to_owned())
+    .collect::<Vec<String>>();
+    
+    //Filter lines that contain letters known not to be in the result
+    let mut filtered_list = Vec::<String>::new();
+    'outer: for line in &list {
+        for c in nons.chars() {
+            if !line.contains(c) {
+                continue 'outer;
+            }
+        }
+        filtered_list.push(line.to_string());
+    }
+
+    //Filter lines that do not contain letters known to be in the result
+    list = filtered_list.clone();
+    filtered_list.clear();
+    'outer: for line in &list {
+        for c in unknown.chars() {
+            if line.contains(c) {
+                continue 'outer;
+            }
+        }
+        filtered_list.push(line.to_string());
+    }
+
+    //Filter lines with the exact pattern given in the knowns
+    list = filtered_list.clone();
+    filtered_list.clear();
+    'outer: for line in &list {
+        let mut idx = 0;
+        for c in known.chars() {
+            if c != '*'
+                && c != line.chars().nth(idx).unwrap() {
+                    continue 'outer;
+            }
+            idx += 1;
+        }
+        filtered_list.push(line.to_string());
+    }
+
+    Some(filtered_list)
 }
