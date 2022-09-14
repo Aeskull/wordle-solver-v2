d4cc7f091b8b0bfeb574acb7c764cc23b237904f
Aeskul <aeskulstyr@gmail.com>
2022-09-14 13:43:55 -0600

    Updating Done; TODO: Solve

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(string_remove_matches)]
+
 mod solver;
 use inquire::Select;
 
@@ -6,9 +8,9 @@ fn menu(items: &[String]) -> State {
     match Select::new("MENU", items.to_vec())
     .prompt()
     .unwrap().as_str() {
-        "Update Known Locations" => KNOWN,
-        "Update Unknown Characters" => UNKNOWN,
-        "Update Known Non-Characters" => NON,
+        "KNOWNS" => KNOWN,
+        "UNKNOWNS" => UNKNOWN,
+        "NONS" => NON,
         "SOLVE" => SOLVING,
         "EXIT" => EXIT,
         _ => EXIT,
@@ -17,7 +19,6 @@ fn menu(items: &[String]) -> State {
 
 #[derive(PartialEq, Eq)]
 enum State {
-    READY,
     KNOWN,
     UNKNOWN,
     NON,
@@ -26,22 +27,48 @@ enum State {
 }
 
 fn main() {
-    let m_items = ["Update Known Locations".to_owned(), 
-    "Update Unknown Characters".to_owned(), 
-    "Update Known Non-Characters".to_owned(), 
+    let m_items = ["KNOWNS".to_owned(), 
+    "UNKNOWNS".to_owned(), 
+    "NONS".to_owned(), 
     "SOLVE".to_owned(), 
     "EXIT".to_owned()];
 
     let mut known = "*****".to_owned();
+    let mut unknown = String::new();
+    let mut non = String::new();
     let mut state = menu(&m_items);
     loop {
         match state {
             State::KNOWN => {
-                solver::update_known(&mut known).unwrap();
-                println!("The new knowns: {}", known);
+                match solver::update_known(&mut known) {
+                    Ok(_) => {
+                        println!("The new knowns: \"{}\"", known);
+                    },
+                    Err(_) => {
+                        println!("The knowns are still \"{}\"", known);
+                    },
+                };
             },
-            State::UNKNOWN => {},
-            State::NON => {},
+            State::UNKNOWN => {
+                match solver::update_unknown_nons(&mut unknown, false) {
+                    Ok(_) => {
+                        println!("The new unknowns: \"{}\"", unknown);
+                    }
+                    Err(_) => {
+                        println!("THe unknowns are still \"{}\"", unknown);
+                    }
+                };
+            },
+            State::NON => {
+                match solver::update_unknown_nons(&mut non, true) {
+                    Ok(_) => {
+                        println!("The new nons are: \"{}\"", non);
+                    }
+                    Err(_) => {
+                        println!("The nons are still \"{}\"", non);
+                    }
+                };
+            },
             State::SOLVING => {},
             _ => break,
         };
--- src/solver.rs
+++ src/solver.rs
@@ -1,13 +1,15 @@
-use std::io::{stdin, stdout, Write};
+use std::io::stdin;
 use inquire::Select;
 
 pub fn update_known(inp: &mut String) -> Result<(), ()> {
-    println!("{inp}");
-    print!("Please type what the known characters are. Denote unknown characters with '*'\n>> ");
+    println!("Current knowns: {inp}");
+    print!("Please type what the known characters are. Denote unknown characters with '*'.\n>> ");
+
     let mut new = String::new();
     stdin().read_line(&mut new).unwrap();
     new = new.trim().to_owned();
-    println!("Is this what you want: {new}?");
+
+    println!("Is this what you want: \"{new}\"?");
     let m_items = ["YES".to_owned(), "NO".to_owned()];
 
     match Select::new("CONFIRM", m_items.to_vec())
@@ -19,4 +21,55 @@ pub fn update_known(inp: &mut String) -> Result<(), ()> {
     };
 
     Ok(())
+}
+
+pub fn update_unknown_nons(inp: &mut String, non: bool) -> Result<(), ()> {
+    let kind: String;
+    if non {
+        kind = "nons".to_owned();
+    } else {
+        kind = "unknowns".to_owned();
+    }
+    println!("Current {}: {inp}", kind);
+    print!("Please type any letters to append. Prefix letters being removed with '-'.\n>> ");
+
+    let mut new = String::new();
+    stdin().read_line(&mut new).unwrap();
+    new = new.trim().to_owned();
+
+    let mut rm = false;
+    let mut rms = String::new();
+    let mut con = inp.clone();
+    for c in new.chars() {
+        if c == '-' {
+            rm = true;
+            continue;
+        }
+        else if rm {
+            rms.push(c);
+            continue;
+        }
+        else {
+            con.push(c);
+        }
+    }
+
+    rms.chars().for_each(|c| con.remove_matches(c));
+
+    println!("Is this what you want: \"{con}\"?");
+    let m_items = ["YES".to_owned(), "NO".to_owned()];
+
+    match Select::new("CONFIRM", m_items.to_vec())
+    .prompt()
+    .unwrap().as_str() {
+        "YES" => *inp = con.clone(),
+        "NO" => return Err(()),
+        _ => return Err(()),
+    };
+
+    Ok(())
+}
+
+pub fn solve(known: &String, unknown: &String, nons: &String) -> String {
+    todo!();
 }
